71d38b1e9dd732a345cadf0b6de82df8657b9cc6
BreadGhoti1 <148354667+BreadGhoti1@users.noreply.github.com>
2023-12-19 09:28:05 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,13 +10,13 @@ sudo apt update
 sudo apt install -y jq
 
 mkdir Save
-cp -r DesktopOnCodespacesFix/root/config/* Save
+cp -r DesktopOnCodespaces/root/config/* Save
 
 json_file="DesktopOnCodespaces/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
-    docker run -d --name=DesktopOnCodespacesFix -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=Desktop-Fix -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped desktoponcodespaces
+    docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped desktoponcodespaces
 else
-    docker run -d --name=DesktopOnCodespacesFix -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=Desktop-Fix -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped desktoponcodespaces
+    docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped desktoponcodespaces
 fi
 clear
 echo "INSTALL FINISHED! Check Port Tab"
