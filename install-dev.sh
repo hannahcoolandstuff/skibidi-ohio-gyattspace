f543199b311a8b9497ecb45e8ba06c52e1e37789
BreadGhoti1 <148354667+BreadGhoti1@users.noreply.github.com>
2023-12-19 13:31:44 -0600

    Update install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -1,4 +1,4 @@
-git clone https://git.mollomm1.dev/Mollomm1/DesktopOnCodespaces -b dev
+git clone https://github.com/3kh0/DesktopOnCodespaces -b dev
 cd DesktopOnCodespaces
 pip install textual
 sleep 2
