7471c2ac9feaee22d7c1e229fec979ef1eaa5c4b
BreadGhoti1 <148354667+BreadGhoti1@users.noreply.github.com>
2023-12-19 09:55:34 -0600

    Update install-dev.sh

--- install-dev.sh
+++ install-dev.sh
@@ -12,7 +12,7 @@ sudo apt install -y jq
 mkdir Save
 cp -r DesktopOnCodespacesFixish/root/config/* Save
 
-json_file="DesktopOnCodespaces/options.json"
+json_file="DesktopOnCodespacesFixish/options.json"
 if jq ".enablekvm" "$json_file" | grep -q true; then
     docker run -d --name=DesktopOnCodespaces -e PUID=1000 -e PGID=1000 --device=/dev/kvm --security-opt seccomp=unconfined -e TZ=Etc/UTC -e SUBFOLDER=/ -e TITLE=GamingOnCodespaces -p 3000:3000 --shm-size="2gb" -v $(pwd)/Save:/config --restart unless-stopped desktoponcodespaces
 else
